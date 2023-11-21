c55eb72e024c8fcf7b235a4749396abecf4cbab8
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2023-11-21 16:51:48 +0530

    Update HealthSystem.cs
    HealthSystem is now an interface.

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,32 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HealthSystem
+public interface HealthSystem
 {
-    private int healthCount;
+    public void StartHealth(int number);
 
-    public void StartHealth()
-    {
-        //Add logic.
-    }
+    public void Heal(int number);
 
-    public void Heal(int number)
-    {
-        healthCount += number;
-    }
+    public void Damage(int number);
 
-    public void Damage(int number)
-    {
-        healthCount -= number;
-
-        if(healthCount <= 0)
-          KillEntity();
-    }
-
-    public void KillEntity()
-    {
-        //Kill the entity.
-    }
+    public void Kill();
 }
