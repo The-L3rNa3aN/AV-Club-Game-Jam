c268e16535b41ab635fc2556152b73bc09a01f2e
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2023-11-21 16:53:58 +0530

    Update BaseEntity.cs
    Added the abstract functions from HealthSystem into BaseEntity.

--- Assets/Scripts/BaseEntity.cs
+++ Assets/Scripts/BaseEntity.cs
@@ -7,12 +7,16 @@ public class BaseEntity : MonoBehaviour
 {
     protected const float G_CONSTANT = -20f;
 
+    public int hitsLeft = 10;
+    public int startHits = 10;
     protected Vector3 movementVector;
     protected CharacterController controller;
     
     public virtual void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        StartHealth(startHits);
     }
 
     private void Update()
@@ -30,4 +34,18 @@ public class BaseEntity : MonoBehaviour
         Vector3 _move = new Vector3(movementVector.x, movementVector.y * Time.deltaTime, movementVector.z);
         controller.Move(_move);
     }
+
+    public virtual void StartHealth(int number) => hitsLeft = startHits;
+
+    public virtual void Heal(int number) => hitsLeft += number;
+
+    public virtual void Damage(int number)
+    {
+        hitsLeft -= number;
+
+        if(hitsLeft <= 0)
+            Kill();
+    }
+
+    public virtual void Kill() => gameObject.SetActive(false);
 }
