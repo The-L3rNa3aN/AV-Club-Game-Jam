5e81e8fb87133ba672a7e73c359e9714900f4226
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2023-11-23 13:50:21 +0530

    Update SavePoint.cs
    Work on save points. Works so far.

--- Assets/Scripts/SavePoint.cs
+++ Assets/Scripts/SavePoint.cs
@@ -6,9 +6,39 @@ namespace AVClub
 {
     public class SavePoint : MonoBehaviour
     {
+        public bool isTouching = false;
+        public bool isUsed = false;                 //To prevent calling the SetSavePoint method a million times.
+        private Player _player;
+
+        private void Update()
+        {
+            if(_player != null || _player != default)
+            {
+                if(isTouching && !isUsed && _player.isInteracting)
+                {
+                    GameManager.instance.SetSavePoint(_player, transform.position);
+                    isUsed = true;
+                }
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-            var _player = other.GetComponent<Player>();
+            if(other.GetComponent<Player>())
+            {
+                _player = other.GetComponent<Player>();
+                isTouching = true;
+            }
+        }
+
+        private void OnTriggerExit()
+        {
+            if(_player)
+            {
+                _player = default;
+                isTouching = false;
+                isUsed = false;
+            }
         }
     }
 }
