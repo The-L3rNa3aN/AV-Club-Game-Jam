53eb4ba6816302d2045e0f58f4ba9d3a039356f0
Veman Jadhav <vemancraft@gmail.com>
2023-12-02 18:03:49 +0530

    Work on lore points.
    Using inactive TMP Text gameobjects situated as a child in each lore point. I'm not really sure if that's a good idea.

--- Assets/Scripts/LorePoint.cs
+++ Assets/Scripts/LorePoint.cs
@@ -2,19 +2,40 @@ using AVClub.Bases;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using AVClub.UI;
+using AVClub.Managers;
 
 namespace AVClub
 {
     public class LorePoint : BaseInteractable
     {
+        public TMP_Text loreText;
+        [HideInInspector] public TMP_Text loreTextObject;
+        [HideInInspector] public GameObject loreTextParent;
+
+        private void Start()
+        {
+            loreTextObject = GameManager.instance._gUi.loreText;
+            loreTextParent = GameManager.instance._gUi.ltParent.gameObject;
+
+            loreText.gameObject.SetActive(false);
+        }
+
         public override void OnEnter(Collider other)
         {
             base.OnEnter(other);
+
+            loreTextParent.SetActive(true);
+            loreTextObject.text = loreText.text;
         }
 
         public override void OnExit()
         {
             base.OnExit();
+
+            loreTextParent.SetActive(false);
         }
     }
 }
--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using AVClub.Managers;
+using TMPro;
 
 namespace AVClub.UI
 {
@@ -10,9 +11,17 @@ namespace AVClub.UI
         public GameObject gameScreen;
         public GameObject pauseScreen;
 
+        public TMP_Text loreText;
+        [HideInInspector] public Transform ltParent;
+
+        private void Start()
+        {
+            ltParent = loreText.transform.parent;
+            ltParent.gameObject.SetActive(false);
+        }
+
         public void ToggleScreens(bool state)
         {
-            Debug.Log("TOGGLESCREENS");
             gameScreen.SetActive(state);
             pauseScreen.SetActive(!state);
         }
